81d4a52015cf08b8cabcc72af72513b03f957467
emarinelli <elena.marinelli@gmail.com>
2013-02-24 00:03:57 -0800

    Update NewRepo.java
    Added Javadoc comment up top.

--- NewRepo.java
+++ NewRepo.java
@@ -1,4 +1,8 @@
-public class NewRepo{
+/**
+ * Practice tracking changes in class NewRepo.
+ */
+ 
+ public class NewRepo{
    public static void main(String[] args){
       System.out.println("Please let this work!");
       System.out.println("Just wait until we start the actual project.");
