4270b38a6ef21f836eccc36c8bd9d8bfc403adfc
emarinelli <elena.marinelli@gmail.com>
2013-02-23 23:34:52 -0800

    Update NewRepo.java
    Edited in GitHub.

--- NewRepo.java
+++ NewRepo.java
@@ -1,6 +1,7 @@
 public class NewRepo{
    public static void main(String[] args){
       System.out.println("Please let this work!");
+      System.out.println("Just wait until we start the actual project.");
    }
 }
 
